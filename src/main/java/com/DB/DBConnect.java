f98cad18017f9d92f0abf493976af42c8382f220
Yash Bhargav <yashbhargav4444@gmail.com>
2026-03-08 18:12:39 +0530

    connected tidb database

--- src/main/java/com/DB/DBConnect.java
+++ src/main/java/com/DB/DBConnect.java
@@ -4,25 +4,25 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnect {
-	
-	private static Connection conn;
-	public static Connection getConn() {
-		
-		try {
-			if(conn==null) {
-				Class.forName("com.mysql.cj.jdbc.Driver");
-				conn = DriverManager.getConnection("jdbc:mysql://3JqTXwyACDnwyxu.root:tdguv89hObW8IAUF@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/job_portal?sslMode=VERIFY_IDENTITY");
-				
-			}
-			
-			
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		
-		
 
-		
-		
-		return conn;}
+    private static Connection conn;
+
+    public static Connection getConn() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            conn = DriverManager.getConnection(
+                "jdbc:mysql://gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/job_portal?sslMode=VERIFY_IDENTITY",
+                "3JqTXwyACDnwyxu.root",
+                "tdguv89hObW8IAUF"
+            );
+
+            System.out.println("Database connected!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
 }
